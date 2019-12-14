5469e5355d6550fd16644b436d643cf2debcc615
andre6639 <andredenais@me.com>
2019-12-14 16:01:59 -0500

    Navbar Rubylinks and styling, Homepage and Signup Button

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,10 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery3
+//= require jquery_ujs
+//= require popper
+//= require bootstrap
 //= require activestorage
-//= require jquery
-//= require jquery-ujs
-//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
